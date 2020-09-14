20c1c644f61731d7b57ddbc894f688c2a78589ce
David <awsone115@gmail.com>
2020-09-13 22:10:38 -0400

    Add project files.

---
+++ TestChain/Blockchain.cpp
@@ -0,0 +1,21 @@
+#include "Blockchain.h"
+#include <time.h>
+#include <sstream>      // std::stringstream
+#include <iostream>     // std::cout
+
+
+Blockchain::Blockchain() {
+	_vChain.emplace_back(Block(0, "Genesis Block"));
+	_nDifficulty = 6;
+
+}
+
+void Blockchain::AddBlock(Block bNew) {
+	bNew.sPrevHash = _GetLastBlock().GetHash();
+	bNew.MineBlock(_nDifficulty);
+	_vChain.push_back(bNew);
+}
+
+Block Blockchain::_GetLastBlock() const {
+	return _vChain.back();
+}
